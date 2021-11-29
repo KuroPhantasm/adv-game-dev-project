b7ff8475f33c242cf18ef400f1b86259b64f6ffa
KuroPhantasm <54336800+KuroPhantasm@users.noreply.github.com>
2021-11-29 06:41:46 -0500

    chonky

--- Adv Game Phys Project/Assets/Scripts/Movement.cs
+++ Adv Game Phys Project/Assets/Scripts/Movement.cs
@@ -1,14 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Movement : MonoBehaviour
 {
     private Rigidbody rb;
-    public float speed = 5f;
+    private int count;
+    public float speed = 10f;
+    public bool cubeGrounded = true;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
+    
 
-    private void Move(float input)
+    private void Start()
     {
-        rb.AddForce(transform.forward * input * speed, ForceMode.Force);
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") && cubeGrounded)
+        {
+            rb.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "points")
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        if (other.gameObject.tag == "gg")
+        {
+            loseTextObject.SetActive(true);
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+
+        if (count >= 8)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 }
